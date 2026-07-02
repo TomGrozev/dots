d02e834e8007853c944f042fd225faec70abf61f
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2026-07-02 20:23:18 +1000

    fix: oh my zsh install

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
   echo "  Oh My Zsh already installed, skipping"
 else
   echo "  Installing Oh My Zsh..."
-  sh -c '$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)' "" --unattended
+  curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh -s -- --unattended
 fi
 
 # --- Symlink dotfiles ---
