76e2dd84b21b3e400f89d33edd3925df100f0c10
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2026-07-06 20:20:33 +1000

    fix: zsh plugins mkdir command

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ else
 fi
 
 # Install oh-my-zsh plugins
-mkdir -p /usr/local/share/oh-my-zsh-plugins \
+mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom} \
   && git clone --depth 1 https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use \
   && git clone --depth 1 https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat \
   && git clone --depth 1 https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
